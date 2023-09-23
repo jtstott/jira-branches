a0cbf8c5c692746d35c47345b6bb907e2ef7a2eb
John Stott <johntstott@gmail.com>
2023-09-23 11:32:20 +0100

    Support issue fetching with Jira Issue URL

--- src/cli/command_handler.rs
+++ src/cli/command_handler.rs
@@ -6,7 +6,7 @@ use crate::jira::issue;
 
 pub async fn handle_command(cli: Cli, config: AppConfig) -> Result<(), String> {
     match cli.command {
-        cli_parser::Commands::Checkout { issue_id } => { handle_checkout(&config, issue_id).await?; }
+        cli_parser::Commands::Checkout { issue } => { handle_checkout(&config, issue).await?; }
     }
 
     // if let Some(config_path) = cli.config.as_deref() {
@@ -16,9 +16,10 @@ pub async fn handle_command(cli: Cli, config: AppConfig) -> Result<(), String> {
     Ok(())
 }
 
-async fn handle_checkout(config: &AppConfig, issue_id: String) -> Result<(), String> {
-    let issue_details = issue::get_issue(issue_id.as_str(), &config).await;
+async fn handle_checkout(config: &AppConfig, issue: String) -> Result<(), String> {
+    let issue_details = issue::get_issue(issue.as_str(), &config).await;
+    println!("{:?}", issue_details);
     let branch_name = template::interpret_branch_template(&config.config, issue_details?);
-    checkout::checkout_branch(branch_name.as_str());
+    // checkout::checkout_branch(branch_name.as_str());
     Ok(())
 }
--- src/jira/issue.rs
+++ src/jira/issue.rs
@@ -1,9 +1,12 @@
+use std::collections::HashMap;
 use crate::jira::client;
 use serde::{Deserialize, Serialize};
+use url::{Url};
 use crate::app_config::{AppConfig, UserConfig};
 
-pub async fn get_issue(id: &str, config: &AppConfig) -> Result<JiraIssue, String> {
-    let ticket_id = prefix_id(id, &config.config);
+pub async fn get_issue(issue: &str, config: &AppConfig) -> Result<JiraIssue, String> {
+    let id = extract_id(issue);
+    let ticket_id = prefix_id(id.as_str(), &config.config);
     let url = format!("/issue/{}?fields=summary,issuetype", ticket_id);
     let response = client::make_request(
         url,
@@ -28,6 +31,35 @@ fn prefix_id(id: &str, config: &UserConfig) -> String {
     }
 
     id.to_string()
+}
+
+fn extract_id(issue: &str) -> String {
+    let url = Url::parse(issue);
+
+    match url {
+        Ok(u) => {
+            println!("Is URL: {:?}", u);
+            let params: HashMap<_, _> = u.query_pairs().into_owned().collect();
+            if let Some(selected_issue) = params.get("selectedIssue"){
+                println!("PARAMS: {:?}", selected_issue);
+                return String::from(selected_issue)
+            }
+
+            if u.path().starts_with("/browse/") {
+                if let Some(segments) =u.path_segments() {
+                    if let Some(item) = segments.last() {
+                        return String::from(item)
+                    }
+                }
+            }
+
+            u.to_string()
+        }
+        Err(_) => {
+            println!("Is NOT URL: {:?}", issue);
+            String::from(issue)
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
