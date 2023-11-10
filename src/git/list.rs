a54de3b2792e949e0c894edcd1cd920fbda62123
John Stott <johntstott@gmail.com>
2023-11-10 10:28:14 +0000

    Print all branch search results

--- src/git/list.rs
+++ src/git/list.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::error::Error;
-use git2::Repository;
+use git2::{Repository};
 
 pub fn list_branches(issue_id: String) -> Result<(), Box<dyn Error>> {
     let repo = Repository::discover(env::current_dir().unwrap())
@@ -13,17 +13,15 @@ pub fn list_branches(issue_id: String) -> Result<(), Box<dyn Error>> {
     // let result = repo.find_branch("origin/test-101", BranchType::Remote).unwrap();
     // let result = repo.branch_upstream_remote("refs/heads/test-101");
 
-    let mut filter = branches.filter(|b| b.as_ref().unwrap().0.name().unwrap().unwrap().contains(issue_id.as_str()));
-    let r = filter.next().unwrap().unwrap();
-    println!("RESULT: {}", r.0.name().unwrap().unwrap());
+    let filtered = branches.filter(|b| {
+        let branch_name = b.as_ref().unwrap().0.name();
+        branch_name.unwrap().unwrap().contains(issue_id.as_str())
+    });
 
-    // let mut i = 0;
-    // for branch in branches {
-    //     let b = branch.unwrap();
-    //     i += 1;
-    //     println!("Branch {}: {}", i, b.0.name().unwrap().unwrap());
-    //     let d = 1;
-    // }
+    for branch in filtered {
+        let b = branch.unwrap();
+        println!("{}", b.0.name().unwrap().unwrap());
+    }
 
     Ok(())
 }
